1ddd431ddfd76d77460203f6c81070d106fc0958
NicolasM22N <nicolasmanceraleon@gmail.com>
2026-04-21 17:25:39 -0500

    feat: add jwt dependencies and jwt service

---
+++ src/main/java/com/taller/bookstore/security/JwtService.java
@@ -0,0 +1,23 @@
+package com.taller.bookstore.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET = "mysecretkeymysecretkeymysecretkey";
+    private final long EXPIRATION = 86400000;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+}
